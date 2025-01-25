18dfc8f78936fbc1a81a20fec3f078d9f6c41f7d
Radu Gheorghiu <radu.gh@gmail.com>
2025-01-25 02:02:34 +0200

    Updated the transformations for the first df

--- src/main/java/L2_Spark_Java_Dataset_API/Real_World_Examples/Application.java
+++ src/main/java/L2_Spark_Java_Dataset_API/Real_World_Examples/Application.java
@@ -3,6 +3,9 @@ package L2_Spark_Java_Dataset_API.Real_World_Examples;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.jetbrains.annotations.NotNull;
+
+import static org.apache.spark.sql.functions.*;
 
 public class Application {
 
@@ -28,7 +31,40 @@ public class Application {
                 .option("header", true)
                 .load("src/main/resources/L2/students.csv");
 
+
         System.out.println("Done loading files!");
 
+        Dataset<Row> durham_new = buildDurhamDataset(durham_parks);
+
+        durham_new.show(3);
+
+    }
+
+    public static Dataset<Row> buildDurhamDataset(@NotNull Dataset<Row> df) {
+        Dataset<Row> durham;
+        durham = df.withColumn(
+                "park_id",
+                concat(
+                        df.col("datasetId"),
+                        lit("_"),
+                        df.col("fields.objectid"),
+                        lit("_"),
+                        lit("Durham")
+                        )
+                )
+                .withColumn("park_name", df.col("fields.park_name"))
+                .withColumn("city", lit("Durham"))
+                .withColumn("has_playground", df.col("fields.playground"))
+                .withColumn("zipcode", df.col("fields.zip"))
+                .withColumn("land_in_acres", df.col("fields.acres"))
+                .withColumn("geoX", df.col("geometry.coordinates").getItem(0))
+                .withColumn("geoY", df.col("geometry.coordinates").getItem(1))
+                .drop("fields")
+                .drop("geometry")
+                .drop("record_timestamp")
+                .drop("recordid")
+                .drop("datasetid");
+
+        return durham;
     }
 }
